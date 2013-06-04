455fb21622565e4771405d5b822346e08fb1482e
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2013-06-03 18:32:18 -0700

    Finalize NDNS zone creation. Implement listing of the zone content
    When zone is created, a SOA record is automatically generated. In
    addition to that, ndns.py creates a fully-formatted and signed content
    object, which contains a valid DNS message in wire format, containing
    this SOA record as part of the ANSWER section

--- lib/ndns/ndns.py
+++ lib/ndns/ndns.py
@@ -13,7 +13,9 @@ import sqlite3, iscpy
 import cStringIO
 import pyccn
 import dns.rrset
+import dns.message
 from pyccn import _pyccn
+import time
 
 class ndns:
     __slots__ = ["_config", "_db"]
@@ -55,6 +57,9 @@ class ndns:
         except:
             pass
 
+        self._db.isolation_level = "DEFERRED"
+        self._db.execute ("BEGIN TRANSACTION");
+
     def __del__ (self):
         self._db.commit ()
         self._db.close ()
@@ -66,6 +71,11 @@ class ndns:
         row = c.fetchone ()
         return None if not row else row[0]
 
+    def listZones (self):
+        c = self._db.cursor ()
+        for row in c.execute ("SELECT id, name FROM zones ORDER BY name"):
+            yield pyccn.Name (ccnb_buffer = row[1])
+
     def createZone (self, zone_name):
         c = self._db.cursor ()
         c.execute ("INSERT INTO zones (name) VALUES(?)", [buffer (zone_name.get_ccnb ())])
@@ -75,43 +85,72 @@ class ndns:
         c = self._db.cursor ()
         c.execute ("DELETE FROM zones WHERE id = ?", [zone_id])
 
+    def listRrSets (self, zone_id):
+        c = self._db.cursor ()
+        for rrset in c.execute ("SELECT id, label, class, type, ndndata FROM rrsets WHERE zone_id = ?", [zone_id]):
+            co = pyccn.ContentObject.from_ccnb (rrset[4])
+            # print co.content
+            msg = dns.message.from_wire (co.content)
+            yield {"id": rrset[0], "rrset":msg, "data":co, "label":rrset[1]}
+        
     def addRR (self, zone_id, label, ttl, rdata):
         c = self._db.cursor ()
-        
-        # find or create RRset
-        c.execute ('''SELECT id,name FROM rrsets JOIN zones on zones.id = rrsets.zone_id 
-                         WHERE zone_id = ? AND label = ? AND class = ? AND type = ?''',
-                   [zone_id, label, rdata.rdclass, rdata.rdtype])
-        row = c.fetchone ()
-        if row:
-            rrset_id = row[0]
-            zone_name = pyccn.Name (ccn_data = buffer (row[1]))
-        else:
-            c.execute ("INSERT INTO rrsets (zone_id, label, class, type) VALUES (?, ?, ?, ?)",
+
+        try:
+            # find or create RRset
+            c.execute ('''SELECT zones.id,name FROM rrsets JOIN zones on zones.id = rrsets.zone_id 
+                             WHERE zone_id = ? AND label = ? AND class = ? AND type = ?''',
                        [zone_id, label, rdata.rdclass, rdata.rdtype])
-            rrset_id = c.lastrowid
+            row = c.fetchone ()
+            if row:
+                rrset_id = row[0]
+                zone_name = pyccn.Name (ccnb_buffer = row[1])
+            else:
+                c.execute ("INSERT INTO rrsets (zone_id, label, class, type) VALUES (?, ?, ?, ?)",
+                           [zone_id, label, rdata.rdclass, rdata.rdtype])
+                rrset_id = c.lastrowid
+    
+                c.execute ('''SELECT name FROM zones WHERE id = ?''', [zone_id])
+                zone_name = pyccn.Name (ccnb_buffer = c.fetchone ()[0])
+    
+            ##
+    
+            rdata_wire = cStringIO.StringIO ()
+            rdata.to_wire (rdata_wire)
+    
+            c.execute ("INSERT INTO rrs (rrset_id, ttl, rrdata) VALUES (?, ?, ?)",
+                       [rrset_id, ttl, buffer (rdata_wire.getvalue ())])
+            rr_id = c.lastrowid
+            
+            key = pyccn.Key.getDefaultKey ()
+            keyLocator = pyccn.KeyLocator.getDefaultKeyLocator ()
+    
+            rrset = dns.rrset.RRset (dns.name.from_text (label), rdata.rdclass, rdata.rdtype)
+            for row in c.execute ("SELECT ttl,rrdata FROM rrs WHERE rrset_id = ? ORDER BY rrdata", [rrset_id]):
+                rrset.add (ttl = row[0], rd = dns.rdata.from_wire (rdata.rdclass, rdata.rdtype, row[1], 0, len (row[1])))
+    
+            # print rrset.to_text (relativize=False)
+    
+            rrset_name = pyccn.Name (zone_name)
+            if (len (label) > 0):
+                rrset_name = rrset_name.append (label) 
+            rrset_name = rrset_name.append ("dns")
+            rrset_name = rrset_name.append (dns.rdatatype.to_text (rdata.rdtype))
+            
+            signedInfo = pyccn.SignedInfo (key_digest = key.publicKeyID, key_locator = keyLocator, 
+                                           freshness = ttl)
+            # , py_timestamp = time.mktime (time.gmtime()))
+    
+            msg = dns.message.Message (id=0)
+            msg.answer.append (rrset)
 
-            c.execute ('''SELECT name FROM zones WHERE id = ?''', [zone_id])
-            zone_name = pyccn.Name (ccn_data = r.fetchone ()[0])
+            co = pyccn.ContentObject (name = rrset_name, signed_info = signedInfo, content = msg.to_wire ())
+            co.sign (key)
+    
+            c.execute ("UPDATE rrsets SET ndndata = ? WHERE id = ?", [buffer(co.get_ccnb ()), rrset_id])
 
-        ##
+            self._db.commit ()
 
-        rdata_wire = cStringIO.StringIO ()
-        rdata.to_wire (rdata_wire)
-
-        c.execute ("INSERT INTO rrs (rrset_id, ttl, rrdata) VALUES (?, ?, ?)",
-                   [rrset_id, ttl, buffer (rdata_wire.getvalue ())])
-        rr_id = c.lastrowid
-        
-        key = pyccn.Key.getDefaultKey ()
-        keyLocator = pyccn.KeyLocator.getDefaultKeyLocator ()
-
-        rrset = dns.rrset.RRset (dns.name.from_text (label), rdata.rdclass, rdata.rdtype)
-        for row in c.execute ("SELECT ttl,rrdata FROM rrs WHERE rrset_id = ? ORDER BY rrdata", [rrset_id]):
-            rrset.add (ttl = row[0], rd = dns.rdata.from_wire (rdata.rdclass, rdata.rdtype, row[1], 0, len (row[1])))
-
-        # print rrset.to_text (relativize=False)
-
-        # NDN name
-        rrset_ndn_name = 
-        pyccn.ContentObject ()
+        except Exception, e:
+            self._db.rollback ()
+            raise e
