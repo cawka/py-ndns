2ce6fcacb40e3eae95cf602fdb57279f09085afa
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2013-06-28 14:56:05 -0700

    Simple implementation of general identity-based policy verifier
    Policy can be specified as a list of regular expression mappings
    namespace-namespace mappings that are used to authorize key and data
    
    The current implementation is highly ineffective, since it is using
    string-based regular expression mapping.

---
+++ debug/debug-trust-policy.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- Mode:python; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+# 
+# Copyright (c) 2013, Regents of the University of California
+#                     Alexander Afanasyev
+# 
+# BSD license, See the doc/LICENSE file for more information
+# 
+# Author: Alexander Afanasyev <alexander.afanasyev@ucla.edu>
+# 
+
+# make sure modules from lib/ are loaded
+import sys; sys.path = ["%s/../lib" % sys.path[0]] + sys.path
+
+import argparse, time, logging, logging.handlers
+import ndns
+from ndns.policy.identity import *
+import pyccn
+
+_LOG = logging.getLogger ()
+_LOG.setLevel (logging.DEBUG)
+
+_handler = logging.StreamHandler (sys.stderr)
+_handler.setLevel (logging.DEBUG)
+_handler.setFormatter (logging.Formatter('%(asctime)s %(name)s [%(levelname)s]  %(message)s', '%H:%M:%S'))
+_LOG.addHandler (_handler)
+
+TrustPolicy = IdentityPolicy (
+    anchors = [[pyccn.Name ("/ndn/keys/ucla.edu/alex/%C1.M.K%00F%8D%E9%C3%EE4%7F%C1Mjqro%C6L%8DGV%91%90%03%24%ECt%95n%F3%9E%A6i%F1%C9"), 
+                pyccn.Name ("/"),
+                pyccn.Key.createFromPEM (public = """-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDSPdPM7+DjDcUGHtwEDmkq4kO5
+tEUI05w5gR4JC1UiZxS0ckMWSLRPWXozHrpJsjNzDeI6OiQrXzup1tF2IN+Xtdr+
+Pr3CwyBRloTJJbm5kf+pGuJh4fE9Qk0i/fS9Xs6gFup3oPnr+wFFjJObnRTrUsaM
+8TQokOLYZFsatsZOvwIDAQAB
+-----END PUBLIC KEY-----""")
+                ]],
+    rules = [["^((/[^/]+)*)/DNS((/[^/]+)*)/[^/]+/NDNCERT$", "\\1\\3"]]
+    )
+
+if( __name__ == '__main__' ):
+
+    ndn = pyccn.CCN ()
+    ndn.defer_verification (True)
+    co = ndn.get (pyccn.Name ("/ndn/DNS/test/A"))
+
+    print TrustPolicy.verify (co)
---
+++ lib/ndns/policy/identity.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- Mode:python; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+# 
+# Copyright (c) 2013, Regents of the University of California
+#                     Alexander Afanasyev
+# 
+# BSD license, See the doc/LICENSE file for more information
+# 
+# Author: Alexander Afanasyev <alexander.afanasyev@ucla.edu>
+# 
+
+import pyccn
+import re, logging, logging.handlers, sys
+
+class IdentityPolicy:
+    def __init__ (self, anchors = [], rules = [], chain_limit = 10):
+        self._ndn = pyccn.CCN ()
+        self._ndn.defer_verification (True)
+
+        self.anchors = anchors
+        self.rules = rules
+        self.chain_limit = chain_limit
+
+        self._LOG = logging.getLogger ("ndns.policy.Identity")
+
+    def verify (self, dataPacket):
+        if len (self.anchors) == 0:
+            return False
+
+        limit_left = self.chain_limit
+        
+        while (limit_left > 0):
+            data_name = dataPacket.name
+            key_name = dataPacket.signedInfo.keyLocator.keyName
+        
+            anchor = self.authorize_by_anchor (data_name, key_name)
+            if anchor:
+                return dataPacket.verify_signature (anchor)
+
+            if not self.authorize_by_rule (data_name, key_name):
+                return False
+
+            keyDataPacket = self._ndn.get (key_name, timeoutms = 3000)
+            key = pyccn.Key.createFromDER (public = keyDataPacket.content)
+            verified = dataPacket.verify_signature (key)
+                
+            if not verified:
+                return False
+
+            limit_left -= 1
+            dataPacket = keyDataPacket
+            # continue
+
+        return False
+
+
+    def authorize_by_anchor (self, data_name, key_name):
+        self._LOG.debug ("== authorize_by_anchor == data: [%s], key: [%s]" % (data_name, key_name))
+
+        if not isinstance (data_name, pyccn.Name):
+            data_name = pyccn.Name (data_name)
+
+        if not isinstance (key_name, pyccn.Name):
+            key_name = pyccn.Name (key_name)
+
+        for anchor in self.anchors:
+            if key_name == anchor[0]:
+                namespace_key = anchor[1]
+                if namespace_key[:] == data_name[0:len (namespace_key)]:
+                    return anchor[2]
+                
+        return None
+
+    def authorize_by_rule (self, data_name, key_name):
+        self._LOG.debug ("== authorize_by_rule == data: [%s], key: [%s]" % (data_name, key_name))
+
+        if not isinstance (data_name, str):
+            data_name = str (data_name)
+
+        if not isinstance (key_name, str):
+            key_name = str (key_name)
+
+        for rule in self.rules:
+            matches_key = re.match (rule[0], key_name)
+            if matches_key:
+                matches_data = re.match (rule[0], key_name)
+
+                if matches_data:
+                    namespace_key  = matches_key.expand (rule[1])
+                    namespace_data = matches_data.expand (rule[1])
+                    if len(namespace_key) == 0:
+                        namespace_key = "/"
+
+                    if len(namespace_data) == 0:
+                        namespace_data = "/"
+
+                    namespace_key = pyccn.Name (namespace_key)
+                    namespace_data = pyccn.Name (namespace_data)
+                    
+                    if namespace_key[:] == namespace_data[0:len (namespace_key)]:
+                        return True
+        return False
+    
